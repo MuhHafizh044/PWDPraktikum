6e80ad4ab2e3e30e8c865fc8f373c1715c30c622
mhafizz <muhammad1900018044@webmail.uad.ac.id>
2021-10-12 18:50:28 +0700

    PWDPraktikum

--- upload_file/download.php
+++
@@ -1,8 +0,0 @@
-<?php
-$myDir = "c:/xampp/htdocs/PWDPertemuan1/upload_file/";
-$dir = opendir($myDir);
-echo "Isi folder (klik link untuk download : <br>";
-while ($tmp = readdir($dir)) {
-    echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
-}
-closedir($dir);
